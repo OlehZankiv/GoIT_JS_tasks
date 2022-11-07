41dd67696a8159e3a32aec9926d59cdbb3bebdb4
Oleh Zankiv <oleh.zankiv@zealicsolutions.com>
2022-11-07 22:41:52 +0200

    module_6 lection_11 init

---
+++ lection_11/index.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+// Задача 1.
+// 1.1. Створити список технологій що вивчаються на курсі Fullstack
+// за допомогою js, використовуючи два способи(map та reduce).
+
+// const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+
+// Задача 2.
+// Створити декілька кнопок на основі масива з об'єктами,
+// використовуючи createElement.
+
+// const colors = [
+// 	{ label: "red", color: "#FF0000" },
+// 	{ label: "green", color: "#00FF00" },
+// 	{ label: "blue", color: "#0000FF" },
+// 	{ label: "yellow", color: "#FFFF00" },
+// ];
+
+// Задача 3.
+// Створити сайт з розміткою записаною у файлі "example.html",
+// використовуючи лише JS в html файлі має бути лише тег body.
